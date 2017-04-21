a7c6e9c2e663a9a67f7888dfba8d152f5175a92c
101TechOverflow <101techoverflow@gmail.com>
2017-04-21 12:25:25 +0100

    Missing files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+SESSION_START();
+$isLogged = false;
+if(is_numeric(@$_SESSION["id"])){
+	$isLogged = true;
+}
+
+if($isLogged){
+    include("html/index.php");
+}
+else {
+    include("html/login.php");
+}
+
+?>
+
